b1e95aeadcac87d1f209c1b71946514eb36b9d03
OksanaTryhub <108359208+OksanaTryhub@users.noreply.github.com>
2023-02-12 22:08:20 +0200

    Update api-vars.js

--- src/js/api-vars.js
+++ src/js/api-vars.js
@@ -1,4 +1,4 @@
-const API_KEY = '31449444226ba6345698313fe055564a';
+const API_KEY = 'a034e16426b596aeae20a6d68cbc45ef';
 const BASE_URL = 'https://api.themoviedb.org/3';
 const TREND_URL = `${BASE_URL}/trending/movie/week`;
 const SEARCH_URL = `${BASE_URL}/search/movie`;
